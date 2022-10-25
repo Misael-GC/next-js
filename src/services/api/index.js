9ee60c9fa3e420432726f8594b8a8882c99b55d9
Misael-GC <misael.gomez1623@gmail.com>
2022-10-24 20:36:13 -0500

    variables de entorno y endPoints API

---
+++ src/services/api/index.js
@@ -0,0 +1,34 @@
+const API = process.env.NEXT_PUBLIC_API_URL;
+const VERSION = process.env.NEXT_PUBLIC_API_URL_VERSION;
+
+const endPoints = {
+  products: {
+    getProducts: `${API}/api/${VERSION}/products`,
+    postProducts: `${API}/api/${VERSION}/products`,
+    getProduct: (id) => `${API}/api/${VERSION}/products/${id}`,
+    putProduct: (id) => `${API}/api/${VERSION}/products/${id}`,
+    deleteProduct: (id) => `${API}/api/${VERSION}/products/${id}`,
+  },
+  users: {
+    getUsers: `${API}/api/${VERSION}/users`,
+    postUsers: `${API}/api/${VERSION}/users`,
+    postUserA: `${API}/api/${VERSION}/users/is-available`,
+  },
+  auth: {
+    login: `${API}/api/${VERSION}/auth/login`,
+    profile: `${API}/api/${VERSION}/auth/profile`,
+  },
+  categories: {
+    getCategories: `${API}/api/${VERSION}/categories`,
+    postCategories: `${API}/api/${VERSION}/categories`,
+    getCategorie: (id) => `${API}/api/${VERSION}/categories/${id}`,
+    putCategorie: (id) => `${API}/api/${VERSION}/categories/${id}`,
+    getCategorieP: (id) => `${API}/api/${VERSION}/categories/${id}/products`,
+  },
+  files: {
+    postFiles: `${API}/api/${VERSION}/files/upload`,
+    getFiles: (filename) => `${API}/api/${VERSION}/files/${filename}`,
+  },
+};
+
+export default endPoints;
