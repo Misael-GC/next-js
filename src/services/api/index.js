f4948188a1d617099625dba85a5673e686c2075b
Misael-GC <misael.gomez1623@gmail.com>
2022-10-30 20:00:06 -0600

    Obteniendo la lista de productos desde la API

--- src/services/api/index.js
+++ src/services/api/index.js
@@ -2,21 +2,21 @@ const API = process.env.NEXT_PUBLIC_API_URL;
 const VERSION = process.env.NEXT_PUBLIC_API_VERSION;
 
 const endPoints = {
+  auth: {
+    login: `${API}/api/${VERSION}/auth/login`,
+    profile: `${API}/api/${VERSION}/auth/profile`,
+  },
   products: {
-    getProducts: `${API}/api/${VERSION}/products`,
-    postProducts: `${API}/api/${VERSION}/products`,
-    getProduct: (id) => `${API}/api/${VERSION}/products/${id}`,
-    putProduct: (id) => `${API}/api/${VERSION}/products/${id}`,
-    deleteProduct: (id) => `${API}/api/${VERSION}/products/${id}`,
+    getProducts: `${API}/api/${VERSION}/products/`,
+    postProducts: `${API}/api/${VERSION}/products/`,
+    getProduct: (id) => `${API}/api/${VERSION}/products/${id}/`,
+    putProduct: (id) => `${API}/api/${VERSION}/products/${id}/`,
+    deleteProduct: (id) => `${API}/api/${VERSION}/products/${id}/`,
   },
   users: {
     getUsers: `${API}/api/${VERSION}/users`,
     postUsers: `${API}/api/${VERSION}/users`,
     postUserA: `${API}/api/${VERSION}/users/is-available`,
-  },
-  auth: {
-    login: `${API}/api/${VERSION}/auth/login`,
-    profile: `${API}/api/${VERSION}/auth/profile`,
   },
   categories: {
     getCategories: `${API}/api/${VERSION}/categories`,
