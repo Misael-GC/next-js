8b06308f8223a14a84a73ffcc4f5d6832e22fa22
Misael-GC <misael.gomez1623@gmail.com>
2022-11-09 01:52:46 -0600

    cambios

--- next.config.js
+++ next.config.js
@@ -1,17 +1,7 @@
-/** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   reactStrictMode: true,
-//   swcMinify: true,
-// };
-
 module.exports = {
   reactStrictMode: true,
   swcMinify: true,
   eslint: {
     ignoreDuringBuilds: true,
   },
-  images: {
-    domains: ['placeimage.com'],
-  },
-  unoptimized: true,
 };
