de6d4f0a7df6abc2ad672eed448be5d2075ea714
Misael-GC <misael.gomez1623@gmail.com>
2022-11-09 00:34:49 -0600

    Implementación del logout

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -41,10 +41,19 @@ function useProvideAuth() {
       setUser(user);
     }
   };
+
+  const logout = () => {
+    Cookie.remove('token');
+    setUser(null);
+    delete axios.defaults.headers.Authorization;
+    window.location.href = '/login';
+  };
+
   return {
     user,
     error,
     setError,
     singIn,
+    logout,
   };
 }
