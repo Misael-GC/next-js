a92be40972acc7019d2e0428f81e5836d0d3050b
Misael-GC <misael.gomez1623@gmail.com>
2022-11-09 00:49:02 -0600

    hacer invisible al componente nav.

--- src/common/Nav.js
+++ src/common/Nav.js
@@ -3,12 +3,18 @@ import { useRouter } from 'next/router';
 export default function Nav() {
   const router = useRouter();
   const route = router.asPath.substring(1);
+  const withNav = ['login', 'dashboard/edit'];
 
-  return (
-    <nav className="bg-white shadow">
-      <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-        <h1 className="text-3xl font-bold text-gray-900 capitalize">{route}</h1>
-      </div>
-    </nav>
-  );
+  for (const iterator of withNav) {
+    if (route.startsWith(iterator)) {
+      return (
+        <nav className="bg-white shadow">
+          <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+            <h1 className="text-3xl font-bold text-gray-900 capitalize">{route}</h1>
+          </div>
+        </nav>
+      );
+    }
+  }
+  return false;
 }
